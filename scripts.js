e88abee0f1262a416d8ceda728a16dbc6918320b
Luiz Capel <lascapel@uem.br>
2025-06-02 16:04:34 -0300

    aprontando para o servidor koyeb

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 
-//  const API = "https://exclusive-krista-luizcapel-78430027.koyeb.app/api";
-  const API = "http://186.233.152.174:8080/api";
+  const API = "https://exclusive-krista-luizcapel-78430027.koyeb.app/api";
+//  const API = "http://186.233.152.174:8080/api";
 //  const API = "http://localhost:8080/api";
 
   let token = null;
